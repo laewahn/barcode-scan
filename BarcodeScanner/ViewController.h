6415db5590ba90e06d5f569ba55b3fd060e96fa4
Dennis Lewandowski <dennis.lewandowski@rwth-aachen.de>
2016-02-02 21:35:15 +0100

    Initial Commit

---
+++ BarcodeScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarcodeScanner
+//
+//  Created by Dennis Lewandowski on 02/02/16.
+//  Copyright © 2016 laewahn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
